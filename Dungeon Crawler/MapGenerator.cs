3ed85058c82d4b12dbf0134ac260ca15138aa755
Marcin Kapelan <marcinkape@gmail.com>
2018-03-16 10:39:57 +0100

    Initial commit

---
+++ Dungeon Crawler/MapGenerator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Crawler
+{
+    class MapGenerator
+    {
+        public int MapWidth;
+        public int MapHeight;
+        public int MinRoomSize;
+        public int MaxRoomSize;
+        public int MaxNumberOfRooms;
+        public int[,] Map;
+
+        public List<Room> Rooms;
+
+        public MapGenerator(int mapWidth, int mapHeight, int minRoomSize, int maxRoomSize, int maxNumberOfRooms)
+        {
+            Map = new int[mapHeight, mapWidth];
+            Rooms = new List<Room>();
+            MapWidth = mapWidth;
+            MapHeight = mapHeight;
+            MinRoomSize = minRoomSize;
+            MaxRoomSize = maxRoomSize;
+            MaxNumberOfRooms = maxNumberOfRooms;
+        }
+
+        public void PlaceRooms()
+        {
+            for (int I = 0; I < MaxNumberOfRooms; I++)
+            {
+                int Width = MinRoomSize + new Random(MaxRoomSize - MinRoomSize + 1).Next();
+                int Height = MinRoomSize + new Random(MaxRoomSize - MinRoomSize + 1).Next();
+                int X = new Random(MapWidth - Width - 1).Next() + 1;
+                int Y = new Random(MapHeight - Height - 1).Next() + 1;
+
+                Room NewRoom = new Room(X, Y, Width, Height);
+                bool Failed = false;
+                foreach (Room Room in Rooms)
+                {
+                    if (NewRoom.Intersects(Room))
+                    {
+                        Failed = true;
+                        break;
+                    }
+                }
+                if (!Failed)
+                {
+                    Rooms.Add(NewRoom);
+                }
+            }
+
+        }
+
+        private int[] fillArray()
+        {
+            foreach (Room room in Rooms)
+            {
+                for (int I = room.Y1; I <= room.Y2)
+            }
+            return null;
+        }
+    }
+}
---
+++ Dungeon Crawler/Room.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Crawler
+{
+    class Room
+    {
+        public int X1;
+        public int X2;
+        public int Y1;
+        public int Y2;
+
+        public int Width;
+        public int Height;
+
+        private Point center;
+
+        public Room(int x, int y, int width, int height)
+        {
+            X1 = x;
+            X2 = x + width;
+            Y1 = y;
+            Y2 = y + height;
+            center = new Point((X1 + X2) / 2, (Y1 + Y2) / 2);
+        }
+
+        public bool Intersects(Room room)
+        {
+            return (X1 <= room.X2 && X2 >= room.X2 &&
+            Y1 <= room.Y2 && room.Y2 >= room.Y1);
+        }
+    }
+}
