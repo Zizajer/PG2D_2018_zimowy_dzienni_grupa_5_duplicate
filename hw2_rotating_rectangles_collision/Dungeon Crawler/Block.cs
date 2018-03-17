fb1ffb51d0d3b4972513ef08557620277ad5fd44
Cezary Witkowski <czarecoo@gmail.com>
2018-03-17 14:49:13 +0100

    Added homework 2 (collision between stick figure and rotating objects)

---
+++ hw2_rotating_rectangles_collision/Dungeon Crawler/Block.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Crawler
+{
+    class Block
+    {
+        public Vector2 Position;
+        public float Rotation;
+        public Block(Vector2 pos, float rot)
+        {
+            Position = pos;
+            Rotation = rot;
+        }
+    }
+    
+}
