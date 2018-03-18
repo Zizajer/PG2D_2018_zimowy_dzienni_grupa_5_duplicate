e92a68184f4b003d34f3ddce60f49c61d2ab1856
Marcin Kapelan <marcinkape@gmail.com>
2018-03-18 23:45:18 +0100

    Add homework 2 [rotatable rectangles collision]

--- Dungeon Crawler/Game1.cs
+++ Dungeon Crawler/Game1.cs
@@ -4,13 +4,21 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Dungeon_Crawler
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
+    // Source: http://www.xnadevelopment.com/tutorials/rotatedrectanglecollisions/rotatedrectanglecollisions.shtml
+    // Algorithm bases on Separating Axes Theorem (SAT).
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+        RotatedRectangle RectangleA;
+        RotatedRectangle RectangleB;
+
+        Texture2D RectangleTexture;
+
+        bool IsRectangleASelected = true;
+
+        KeyboardState PreviousKeyboardState;
 
         public Game1()
         {
@@ -38,7 +46,13 @@ namespace Dungeon_Crawler
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
+
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            RectangleTexture = Content.Load<Texture2D>("Square");
+
+            RectangleA = new RotatedRectangle(new Rectangle(100, 200, 70, 70), 0.0f);
+            RectangleB = new RotatedRectangle(new Rectangle(400, 150, 130, 200), 45.0f);
 
             // TODO: use this.Content to load your game content here
         }
@@ -62,9 +76,66 @@ namespace Dungeon_Crawler
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+
+            SwitchActiveRectangle();
+            MoveRectangle();
 
             base.Update(gameTime);
+        }
+
+
+        private void SwitchActiveRectangle()
+        {
+            KeyboardState aCurrentKeyboardState = Keyboard.GetState();
+            if (aCurrentKeyboardState.IsKeyDown(Keys.Tab) && PreviousKeyboardState.IsKeyDown(Keys.Tab) == false)
+            {
+                IsRectangleASelected = !IsRectangleASelected;
+            }
+            PreviousKeyboardState = aCurrentKeyboardState;
+        }
+
+        private void MoveRectangle()
+        {
+            KeyboardState CurrentKeyboard = Keyboard.GetState();
+            RotatedRectangle RectangleToMove;
+            RotatedRectangle AnotherRectangle;
+
+            if (IsRectangleASelected)
+            {
+                RectangleToMove = RectangleA;
+                AnotherRectangle = RectangleB;
+            }
+            else
+            {
+                RectangleToMove = RectangleB;
+                AnotherRectangle = RectangleA;
+            }
+
+            if (CurrentKeyboard.IsKeyDown(Keys.Up) && !AnotherRectangle.Intersects(new RotatedRectangle(new Rectangle(RectangleToMove.X, RectangleToMove.Y - 2, RectangleToMove.Width, RectangleToMove.Height), RectangleToMove.Rotation)))
+            {
+                RectangleToMove.ChangePosition(0, -2);
+            }
+
+            if (CurrentKeyboard.IsKeyDown(Keys.Down) && !AnotherRectangle.Intersects(new RotatedRectangle(new Rectangle(RectangleToMove.X, RectangleToMove.Y + 2, RectangleToMove.Width, RectangleToMove.Height), RectangleToMove.Rotation)))
+            {
+                RectangleToMove.ChangePosition(0, 2);
+            }
+
+            if (CurrentKeyboard.IsKeyDown(Keys.Left) && !AnotherRectangle.Intersects(new RotatedRectangle(new Rectangle(RectangleToMove.X - 2, RectangleToMove.Y, RectangleToMove.Width, RectangleToMove.Height), RectangleToMove.Rotation)))
+            {
+                RectangleToMove.ChangePosition(-2, 0);
+            }
+
+            if (CurrentKeyboard.IsKeyDown(Keys.Right) && !AnotherRectangle.Intersects(new RotatedRectangle(new Rectangle(RectangleToMove.X + 2, RectangleToMove.Y, RectangleToMove.Width, RectangleToMove.Height), RectangleToMove.Rotation)))
+            {
+                RectangleToMove.ChangePosition(2, 0);
+            }
+
+            if (CurrentKeyboard.IsKeyDown(Keys.Space) && !AnotherRectangle.Intersects(new RotatedRectangle(new Rectangle(RectangleToMove.X, RectangleToMove.Y, RectangleToMove.Width, RectangleToMove.Height), RectangleToMove.Rotation)))
+            {
+                RectangleToMove.Rotation += 0.01f;
+            }
+
         }
 
         /// <summary>
@@ -75,7 +146,18 @@ namespace Dungeon_Crawler
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            Color aColor = Color.Blue;
+            Color bColor = Color.Yellow;
+
+            spriteBatch.Begin();
+
+            Rectangle aPositionAdjusted = new Rectangle(RectangleA.X + (RectangleA.Width / 2), RectangleA.Y + (RectangleA.Height / 2), RectangleA.Width, RectangleA.Height);
+            spriteBatch.Draw(RectangleTexture, aPositionAdjusted, new Rectangle(0, 0, 2, 6), aColor, RectangleA.Rotation, new Vector2(2 / 2, 6 / 2), SpriteEffects.None, 0);
+
+            aPositionAdjusted = new Rectangle(RectangleB.X + (RectangleB.Width / 2), RectangleB.Y + (RectangleB.Height / 2), RectangleB.Width, RectangleB.Height);
+            spriteBatch.Draw(RectangleTexture, aPositionAdjusted, new Rectangle(0, 0, 2, 6), bColor, RectangleB.Rotation, new Vector2(2 / 2, 6 / 2), SpriteEffects.None, 0);
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
