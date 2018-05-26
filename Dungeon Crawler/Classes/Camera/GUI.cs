83189e6e0647458cb9f5593ff91bca1ac70f97b1
Marcin Kapelan <marcinkape@gmail.com>
2018-05-26 15:05:37 +0200

    Add statistics drawn over boss and player, add player-exclusive health and mana bars in upper left corner

--- Dungeon Crawler/Classes/Camera/GUI.cs
+++ Dungeon Crawler/Classes/Camera/GUI.cs
@@ -17,6 +17,9 @@ namespace Dungeon_Crawler
         private double lastMsgGametime = 0;
         private double afterHowLongClearHighestMsg = 4; //sec
 
+        private int HealthBarOverCharacterBackgroundWidth = 50;
+        private int HealthBarOverCharacterHeight = 10;
+
         public GUI(GraphicsDeviceManager graphics, SpriteFont f)
         {
             this.graphics = graphics;
@@ -28,65 +31,55 @@ namespace Dungeon_Crawler
             int tempY = Global.Camera.ViewportWorldBoundry().Y;
             int tempX2 = Global.Camera.ViewportWorldBoundry().X + (int)(Global.Camera.ViewportWorldBoundry().Width * 0.28);
             int tempY2 = Global.Camera.ViewportWorldBoundry().Y + (int)(Global.Camera.ViewportWorldBoundry().Height * 0.07);
-            int tempY3 = Global.Camera.ViewportWorldBoundry().Y + (int)(Global.Camera.ViewportWorldBoundry().Height * 0.21);
+            int tempY3 = Global.Camera.ViewportWorldBoundry().Y + (int)(Global.Camera.ViewportWorldBoundry().Height * 0.14);
+            int tempY4 = Global.Camera.ViewportWorldBoundry().Y + (int)(Global.Camera.ViewportWorldBoundry().Height * 0.21);
+            int PlayerBarsWidth = (int)(Global.Camera.ViewportWorldBoundry().Width * 0.35);
+            int PlayerBarsHeight = (int)(Global.Camera.ViewportWorldBoundry().Height * 0.05);
 
             int gameOverX = (int)(Global.Camera.ViewportWorldBoundry().X+Global.Camera.ViewportWorldBoundry().Width / 2 - font.MeasureString("Game Over").Length() / 2);
             int gameOverY = Global.Camera.ViewportWorldBoundry().Y+Global.Camera.ViewportWorldBoundry().Height / 3;
             float scale = Global.Camera.Zoom;
             if (lm.player.CurrentHealth > 0)
             {
-                string healthString = "Health " + lm.player.CurrentHealth;
-                string manaString = "Mana " + (int)lm.player.Mana;
+                DrawPlayerStatBars(spriteBatch, lm.player, tempX, tempY, tempX, tempY2, 0, 0, PlayerBarsWidth, PlayerBarsHeight, scale);
+                DrawHealthBarOverCharacter(spriteBatch, lm.player);
+
                 string s = lm.player.getItems() + " \nLevel " + (lm.player.CurrentMapLevel + 1);
-                spriteBatch.DrawString(font, healthString, new Vector2(tempX, tempY), Color.OrangeRed, 0.0f, Vector2.One, 1 / scale, SpriteEffects.None, Layers.Text);
-                spriteBatch.DrawString(font, manaString, new Vector2(tempX2, tempY), Color.DeepSkyBlue, 0.0f, Vector2.One, 1 / scale, SpriteEffects.None, Layers.Text);
-                spriteBatch.DrawString(font, s, new Vector2(tempX, tempY2), Color.White, 0.0f, Vector2.One, 1 / scale, SpriteEffects.None, Layers.Text);
+                spriteBatch.DrawString(font, s, new Vector2(tempX, tempY3), Color.White, 0.0f, Vector2.One, 1 / scale, SpriteEffects.None, Layers.Text);
 
                 if (lm.levels[lm.player.CurrentMapLevel].isBossLevel)
                 {
-                    if(lm.levels[lm.player.CurrentMapLevel].enemies.Count>0){
-                        string bossHealth = "Boss Health: " + lm.levels[lm.player.CurrentMapLevel].enemies[0].CurrentHealth;
-                        spriteBatch.DrawString(font, bossHealth, new Vector2(tempX, tempY3), Color.White, 0.0f, Vector2.One, 1 / scale, SpriteEffects.None, Layers.Text);
+
+                    if (lm.levels[lm.player.CurrentMapLevel].enemies.Count>0){
+
+
+                        DrawHealthBarOverCharacter(spriteBatch, lm.levels[lm.player.CurrentMapLevel].enemies[0]);
+
+                        //Draw level
+                        spriteBatch.DrawString(font, lm.levels[lm.player.CurrentMapLevel].enemies[0].Level.ToString(), new Vector2(lm.levels[lm.player.CurrentMapLevel].enemies[0].Position.X + HealthBarOverCharacterBackgroundWidth + 2, lm.levels[lm.player.CurrentMapLevel].enemies[0].Position.Y - 27), Color.Black, 0.0f, Vector2.One, 0.5f / scale, SpriteEffects.None, Layers.Text);
+
+                        //Draw name
+                        spriteBatch.DrawString(font, lm.levels[lm.player.CurrentMapLevel].enemies[0].Name, new Vector2((int)lm.levels[lm.player.CurrentMapLevel].enemies[0].Position.X, (int)lm.levels[lm.player.CurrentMapLevel].enemies[0].Position.Y - 39), Color.Black, 0.0f, Vector2.One, 0.5f / scale, SpriteEffects.None, Layers.Text);
+
+                        //string bossHealth = "Boss Health: " + lm.levels[lm.player.CurrentMapLevel].enemies[0].CurrentHealth;
+                        //spriteBatch.DrawString(font, bossHealth, new Vector2(tempX, tempY3), Color.White, 0.0f, Vector2.One, 1 / scale, SpriteEffects.None, Layers.Text);
                     }
                     else
                     {
-                        string bossHealth = "Boss is dead";
-                        spriteBatch.DrawString(font, bossHealth, new Vector2(tempX, tempY3), Color.White, 0.0f, Vector2.One, 1 / scale, SpriteEffects.None, Layers.Text);
+                        //string bossHealth = "Boss is dead";
+                        //spriteBatch.DrawString(font, bossHealth, new Vector2(tempX, tempY3), Color.White, 0.0f, Vector2.One, 1 / scale, SpriteEffects.None, Layers.Text);
                     }
                     
                 }
                 else
                 {
-                    foreach(Enemy enemy in lm.levels[lm.player.CurrentMapLevel].enemies)
+                    foreach(Character enemy in lm.levels[lm.player.CurrentMapLevel].enemies)
                     {
-                        //Generate healthbar
-                        int HealthBarCurrentHealthWidth = (int)((double)enemy.CurrentHealthPercent / 100 * 50);
-                        int HealthBarBackgroundWidth = 50;
-                        int HealthBarHeight = 10;
-
-                        Texture2D HealthBarCurrentHealthTexture = new Texture2D(graphics.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
-                        Texture2D HealthBarBackgroundTexture = new Texture2D(graphics.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
-                        HealthBarBackgroundTexture.SetData<Color>(new Color[] { Color.Black });
-
-                        if (enemy.CurrentHealthPercent > 65)
-                        {
-                            HealthBarCurrentHealthTexture.SetData<Color>(new Color[] { Color.Green });
-                        }
-                        else if (enemy.CurrentHealthPercent > 35)
-                        {
-                            HealthBarCurrentHealthTexture.SetData<Color>(new Color[] { Color.Orange });
-                        }
-                        else
-                        {
-                            HealthBarCurrentHealthTexture.SetData<Color>(new Color[] { Color.Red });
-                        }
-
                         //Draw healthbar
-                        spriteBatch.Draw(HealthBarBackgroundTexture, new Rectangle((int)enemy.Position.X, (int)enemy.Position.Y - 25, HealthBarBackgroundWidth, HealthBarHeight), Color.White);
-                        spriteBatch.Draw(HealthBarCurrentHealthTexture, new Rectangle((int)enemy.Position.X, (int)enemy.Position.Y - 25, HealthBarCurrentHealthWidth, HealthBarHeight), Color.White);
+                        DrawHealthBarOverCharacter(spriteBatch, enemy);
 
                         //Draw level
-                        spriteBatch.DrawString(font, enemy.Level.ToString(), new Vector2(enemy.Position.X + HealthBarBackgroundWidth + 2, enemy.Position.Y - 27), Color.Black, 0.0f, Vector2.One, 0.5f / scale, SpriteEffects.None, Layers.Text);
+                        spriteBatch.DrawString(font, enemy.Level.ToString(), new Vector2(enemy.Position.X + HealthBarOverCharacterBackgroundWidth + 2, enemy.Position.Y - 27), Color.Black, 0.0f, Vector2.One, 0.5f / scale, SpriteEffects.None, Layers.Text);
 
                         //Draw name
                         spriteBatch.DrawString(font, enemy.Name, new Vector2((int)enemy.Position.X, (int)enemy.Position.Y - 39), Color.Black, 0.0f, Vector2.One, 0.5f / scale, SpriteEffects.None, Layers.Text);
@@ -100,6 +93,89 @@ namespace Dungeon_Crawler
             tempY += (int)(0.8 * Global.Camera.ViewportWorldBoundry().Height);
             string tempString = console[0] + "\n" + console[1] + "\n" + console[2] + "\n" + console[3];
             spriteBatch.DrawString(font, tempString, new Vector2(tempX, tempY), Color.White, 0.0f, Vector2.One, 0.7f * (1 / scale), SpriteEffects.None, Layers.Text);
+        }
+
+        private void DrawHealthBarOverCharacter(SpriteBatch spriteBatch, Character character)
+        {
+            int HealthBarCurrentHealthWidth = (int)((double)character.CurrentHealthPercent / 100 * HealthBarOverCharacterBackgroundWidth);
+
+            Texture2D HealthBarCurrentHealthTexture = new Texture2D(graphics.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            Texture2D HealthBarBackgroundTexture = new Texture2D(graphics.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            HealthBarBackgroundTexture.SetData<Color>(new Color[] { Color.Black });
+
+            if (character.CurrentHealthPercent > 65)
+            {
+                HealthBarCurrentHealthTexture.SetData<Color>(new Color[] { Color.Green });
+            }
+            else if (character.CurrentHealthPercent > 35)
+            {
+                HealthBarCurrentHealthTexture.SetData<Color>(new Color[] { Color.Orange });
+            }
+            else
+            {
+                HealthBarCurrentHealthTexture.SetData<Color>(new Color[] { Color.Red });
+            }
+
+
+
+            spriteBatch.Draw(HealthBarBackgroundTexture, new Rectangle((int)character.Position.X, (int)character.Position.Y - 25, HealthBarOverCharacterBackgroundWidth, HealthBarOverCharacterHeight), Color.White);
+            spriteBatch.Draw(HealthBarCurrentHealthTexture, new Rectangle((int)character.Position.X, (int)character.Position.Y - 25, HealthBarCurrentHealthWidth, HealthBarOverCharacterHeight), Color.White);
+        }
+
+        private void DrawPlayerStatBars(SpriteBatch spriteBatch, Player player, int hpX, int hpY, int manaX, int manaY, int xpX, int xpY, int width, int height, float scale)
+        {
+            int HealthBarCurrentHealthWidth = (int)((double)player.CurrentHealthPercent / 100 * width);
+            int ManaBarCurrentManaWidth = (int)((double)player.CurrentManaPercent / 100 * width);
+
+            Texture2D HealthBarCurrentHealthTexture = new Texture2D(graphics.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            Texture2D HealthBarBackgroundTexture = new Texture2D(graphics.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            HealthBarBackgroundTexture.SetData<Color>(new Color[] { Color.Black });
+
+            Texture2D ManaBarCurrentManaTexture = new Texture2D(graphics.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            Texture2D ManaBarBackgroundTexture = new Texture2D(graphics.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            ManaBarBackgroundTexture.SetData<Color>(new Color[] { Color.Black });
+
+            if (player.CurrentHealthPercent > 65)
+            {
+                HealthBarCurrentHealthTexture.SetData<Color>(new Color[] { Color.Green });
+            }
+            else if (player.CurrentHealthPercent > 35)
+            {
+                HealthBarCurrentHealthTexture.SetData<Color>(new Color[] { Color.Orange });
+            }
+            else
+            {
+                HealthBarCurrentHealthTexture.SetData<Color>(new Color[] { Color.Red });
+            }
+
+            if (player.CurrentManaPercent > 65)
+            {
+                ManaBarCurrentManaTexture.SetData<Color>(new Color[] { Color.Blue });
+            }
+            else if (player.CurrentManaPercent > 35)
+            {
+                ManaBarCurrentManaTexture.SetData<Color>(new Color[] { Color.DarkViolet });
+            }
+            else
+            {
+                ManaBarCurrentManaTexture.SetData<Color>(new Color[] { Color.Red });
+            }
+
+            spriteBatch.Draw(HealthBarBackgroundTexture, new Rectangle(hpX, hpY, width, height), Color.White * 0.5f);
+            spriteBatch.Draw(HealthBarCurrentHealthTexture, new Rectangle(hpX, hpY, HealthBarCurrentHealthWidth, height), Color.White * 0.7f);
+
+            spriteBatch.Draw(ManaBarBackgroundTexture, new Rectangle(manaX, manaY, width, height), Color.White * 0.5f);
+            spriteBatch.Draw(ManaBarCurrentManaTexture, new Rectangle(manaX, manaY, ManaBarCurrentManaWidth, height), Color.White * 0.7f);
+
+            //Draw HP ratio
+            string HealthText = player.CurrentHealth + "/" + player.Health;
+            Vector2 HealthTextOffsetFromCenter = font.MeasureString(HealthText);
+            spriteBatch.DrawString(font, HealthText, new Vector2(hpX + (float)width / 2 - HealthTextOffsetFromCenter.X / 2, hpY), Color.White, 0.0f, Vector2.One, 0.75f / scale, SpriteEffects.None, Layers.Text);
+
+            //Draw mana ratio
+            string ManaText = (int)player.CurrentMana + "/" + player.Mana;
+            Vector2 ManaTextOffsetFromCenter = font.MeasureString(HealthText);
+            spriteBatch.DrawString(font, ManaText, new Vector2(manaX + (float)width / 2 - ManaTextOffsetFromCenter.X / 2, manaY), Color.White, 0.0f, Vector2.One, 0.75f / scale, SpriteEffects.None, Layers.Text);
         }
 
         internal void addLevelMananger(LevelManager levelManager)
