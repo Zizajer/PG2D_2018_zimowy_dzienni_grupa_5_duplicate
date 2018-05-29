fc7c62642c6af9dc554eab767be7e23e6bd11584
Marcin Kapelan <marcinkape@gmail.com>
2018-05-29 23:56:59 +0200

    Add interfaces for attacks

---
+++ Dungeon Crawler/Classes/Characters/Attacks/Interface/IAttack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Crawler
+{
+    public interface IAttack
+    {
+        string Name { get; set; }
+        int Power { get; set; }
+        int Accuracy { get; set; }
+        int CriticalHitProbability { get; set; }
+
+        bool IsSpecial { get; set; }
+        int ManaCost { get; set; }
+    }
+}
---
+++ Dungeon Crawler/Classes/Characters/Attacks/Interface/IUnTargetedAttack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Crawler.Classes.Characters.Attacks.Interface
+{
+    /*
+     * Interface for untargeted attacks (e.g. Exori)
+     */
+    public interface IUnTargetedAttack : IAttack
+    {
+        bool Use(Character attacker);
+    }
+}
