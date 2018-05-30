6667e858b447921d4e1e917e349eb3e057531189
Marcin Kapelan <marcinkape@gmail.com>
2018-05-30 17:54:53 +0200

    Add small fix for attack classes accessibility

--- Dungeon Crawler/Classes/Characters/Attacks/Interface/IUnTargetedAttack.cs
+++ Dungeon Crawler/Classes/Characters/Attacks/Interface/IUnTargetedAttack.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dungeon_Crawler.Classes.Characters.Attacks.Interface
+namespace Dungeon_Crawler
 {
     /*
      * Interface for untargeted attacks (e.g. Exori)
--- Dungeon Crawler/Classes/Characters/Attacks/Pound.cs
+++ Dungeon Crawler/Classes/Characters/Attacks/Pound.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Dungeon_Crawler
 {
-    class Pound : ICharacterTargetedAttack
+    public class Pound : ICharacterTargetedAttack
     {
         public string Name { get; set; }
         public int Power { get; set; }
