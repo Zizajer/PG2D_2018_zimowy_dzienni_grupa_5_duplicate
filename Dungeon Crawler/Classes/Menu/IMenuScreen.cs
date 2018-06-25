1f06000efc58a1505548d7e6913340f65c5673ce
Piotr Sadza <5673@gmail.com>
2018-06-26 00:13:07 +0200

    Added menuScreen Interface and Abstract Class.

---
+++ Dungeon Crawler/Classes/Menu/IMenuScreen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Dungeon_Crawler
+{
+    public interface IMenuScreen
+    {
+        List<Button> buttonsInMenu { get; set; }
+        List<Inputbox>inputBoxesInMenu { get; set; }
+        List<Checkbox> checkBoxesInMenu { get; set; }
+        List<Label> labelsInMenu { get; set; }
+        List<Sprite> spriteInMenu { get; set; }
+
+        void addButton(Vector2 Position, String text);
+
+        void addIputbox(Vector2 Position, String text);
+
+        void addCheckbox(Vector2 Position, String text);
+
+        void addLabel(Vector2 Position, String text);
+
+        void addSprite(Vector2 Position, Texture2D texture);
+
+
+    }
+}
---
+++ Dungeon Crawler/Classes/Menu/MenuScreen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Dungeon_Crawler
+{
+    public abstract class MenuScreen : IMenuScreen
+    {
+        public List<Button> buttonsInMenu { get; set; }
+        public List<Inputbox> inputBoxesInMenu { get; set; }
+        public List<Checkbox> checkBoxesInMenu { get; set; }
+        public List<Label> labelsInMenu { get; set; }
+        public List<Sprite> spriteInMenu { get; set; }
+
+        public void addButton(Vector2 Position, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addCheckbox(Vector2 Position, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addIputbox(Vector2 Position, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addLabel(Vector2 Position, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addSprite(Vector2 Position, Texture2D texture)
+        {
+            throw new NotImplementedException();
+        }
+    }
